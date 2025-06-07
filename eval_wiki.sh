779b8d803fc7c1af6301c5c59f9b370ebf468498
IC-EnzoD-FRA <spidersouris@gmail.com>
2025-06-07 22:07:42 +0200

    update eval.sh folder paths

--- eval_wiki.sh
+++ eval_wiki.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 echo "Checking if the required directories exist..."
-if [ ! -d "_allscores_sbert/wiki" ]; then
+if [ ! -d "results/wiki" ]; then
     echo "Creating directories..."
-    mkdir -p _allscores_sbert/wiki
+    mkdir -p results/wiki
 fi
 
 echo "Running evaluation for Wiki dataset..."
 echo "Evaluating column 2..."
-python eval.py -c "gen" -ac 2 data/eval/wiki/gen/wiki_gen_eval.csv _allscores_sbert/wiki/wiki_gen_eval_out_2.csv
+python eval.py -c "gen" -ac 2 data/eval/wiki/gen/wiki_gen_eval.csv results/wiki/wiki_gen_eval_out_2.csv
 echo "Evaluating column 3..."
-python eval.py -c "gen" -ac 3 data/eval/wiki/gen/wiki_gen_eval.csv _allscores_sbert/wiki/wiki_gen_eval_out_3.csv
+python eval.py -c "gen" -ac 3 data/eval/wiki/gen/wiki_gen_eval.csv results/wiki/wiki_gen_eval_out_3.csv
 echo "Evaluating column 4..."
-python eval.py -c "gen" -ac 4 data/eval/wiki/gen/wiki_gen_eval.csv _allscores_sbert/wiki/wiki_gen_eval_out_4.csv
+python eval.py -c "gen" -ac 4 data/eval/wiki/gen/wiki_gen_eval.csv results/wiki/wiki_gen_eval_out_4.csv
 echo "Evaluating column 11..."
-python eval.py -c "gen" -ac 11 data/eval/wiki/gen/wiki_gen_eval.csv _allscores_sbert/wiki/wiki_gen_eval_out_11.csv
+python eval.py -c "gen" -ac 11 data/eval/wiki/gen/wiki_gen_eval.csv results/wiki/wiki_gen_eval_out_11.csv
 echo "Evaluating column 12..."
-python eval.py -c "gen" -ac 12 data/eval/wiki/gen/wiki_gen_eval.csv _allscores_sbert/wiki/wiki_gen_eval_out_12.csv
+python eval.py -c "gen" -ac 12 data/eval/wiki/gen/wiki_gen_eval.csv results/wiki/wiki_gen_eval_out_12.csv
 echo "Evaluating column 13..."
-python eval.py -c "gen" -ac 13 data/eval/wiki/gen/wiki_gen_eval.csv _allscores_sbert/wiki/wiki_gen_eval_out_13.csv
+python eval.py -c "gen" -ac 13 data/eval/wiki/gen/wiki_gen_eval.csv results/wiki/wiki_gen_eval_out_13.csv
